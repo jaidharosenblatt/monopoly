138db5a725aeed57559ab00fbe8071838c0f4c7b
Jaidha Rosenblatt <jrr59@duke.edu>
2020-04-07 18:30:43 -0700

    fixed numbering for tiles

--- src/ooga/view/Board.java
+++ src/ooga/view/Board.java
@@ -7,7 +7,6 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -23,34 +22,49 @@ public class Board extends BorderPane {
 
     createBoard();
 
-    setTop(createHorizontalTileRow(0, ROW_LENGTH));
-    setBottom(createHorizontalTileRow(ROW_LENGTH * 2, ROW_LENGTH * 3));
-    setLeft(createSideTileRow(ROW_LENGTH, ROW_LENGTH * 2));
-    setRight(createSideTileRow(ROW_LENGTH*3, ROW_LENGTH * 4));
+    //Spent an hour trying to reduce duplicate code here and still left with this :(
+    setTop(createTop());
+    setBottom(createBottom());
+    setRight(createRight());
+    setLeft(createLeft());
 
   }
 
-  private HBox createHorizontalTileRow(int start, int end) {
+  private HBox createTop() {
     HBox hbox = new HBox();
-    for (int i = start; i < end; i++) {
+    for (int i = 0; i < ROW_LENGTH; i++) {
       hbox.getChildren().add(tiles.get(i));
     }
     return hbox;
   }
 
-  private VBox createSideTileRow(int start, int end) {
+  private VBox createRight() {
     VBox vbox = new VBox();
-    for (int i = start; i < end; i++) {
-      Property tile = tiles.get(i);
-      tiles.get(i).setRotate(90);
-      vbox.getChildren().add(tile);
+    for (int i = ROW_LENGTH; i < ROW_LENGTH * 2; i++) {
+      vbox.getChildren().add(tiles.get(i));
+    }
+    return vbox;
+  }
+
+  private HBox createBottom() {
+    HBox hbox = new HBox();
+    for (int i = ROW_LENGTH * 3; i >= ROW_LENGTH * 2; i--) {
+      hbox.getChildren().add(tiles.get(i));
+    }
+    return hbox;
+  }
+
+  private VBox createLeft() {
+    VBox vbox = new VBox();
+    for (int i = ROW_LENGTH * 4; i >= ROW_LENGTH * 3; i--) {
+      vbox.getChildren().add(tiles.get(i));
     }
     return vbox;
   }
 
   private void createBoard() {
     for (int i = 0; i < NUMBER_OF_TILES; i++) {
-      Property property = new Property(Color.BLACK, "hello", 500);
+      Property property = new Property(Color.BLACK, "hello", i);
       tiles.add(property);
     }
   }
