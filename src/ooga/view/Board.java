1cd0b431711db226f911ceeec156b54c82e60fbd
Jaidha Rosenblatt <jrr59@duke.edu>
2020-04-07 20:05:50 -0700

    switched to gridpane for board visualization

--- src/ooga/view/Board.java
+++ src/ooga/view/Board.java
@@ -5,62 +5,41 @@ import java.util.List;
 import javafx.geometry.Insets;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 
-public class Board extends BorderPane {
+public class Board extends GridPane {
 
   private final static int NUMBER_OF_TILES = 40;
-  private final static int ROW_LENGTH = 9;
+  private final static int ROW_LENGTH = 10;
   private List<Property> tiles = new ArrayList<>();
 
   public Board() {
     setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
 
     createBoard();
-
-    //Spent an hour trying to reduce duplicate code here and still left with this :(
-    setTop(createTop());
-    setBottom(createBottom());
-    setRight(createRight());
-    setLeft(createLeft());
-
+    createGrid();
   }
 
-  private HBox createTop() {
-    HBox hbox = new HBox();
+
+  private void createGrid() {
     for (int i = 0; i < ROW_LENGTH; i++) {
-      hbox.getChildren().add(tiles.get(i));
+      add(tiles.get(i), i, 0);
     }
-    return hbox;
+    for (int i = 0; i < ROW_LENGTH; i++) {
+      add(tiles.get(i + ROW_LENGTH), ROW_LENGTH, i);
+    }
+    for (int i = 0; i < ROW_LENGTH; i++) {
+      add(tiles.get(ROW_LENGTH * 3 - i - 1), i, ROW_LENGTH);
+    }
+
+    for (int i = 0; i < ROW_LENGTH; i++) {
+      add(tiles.get(ROW_LENGTH * 4 - i - 1), 0, i);
+    }
+
   }
 
-  private VBox createRight() {
-    VBox vbox = new VBox();
-    for (int i = ROW_LENGTH; i < ROW_LENGTH * 2; i++) {
-      vbox.getChildren().add(tiles.get(i));
-    }
-    return vbox;
-  }
-
-  private HBox createBottom() {
-    HBox hbox = new HBox();
-    for (int i = ROW_LENGTH * 3; i >= ROW_LENGTH * 2; i--) {
-      hbox.getChildren().add(tiles.get(i));
-    }
-    return hbox;
-  }
-
-  private VBox createLeft() {
-    VBox vbox = new VBox();
-    for (int i = ROW_LENGTH * 4; i >= ROW_LENGTH * 3; i--) {
-      vbox.getChildren().add(tiles.get(i));
-    }
-    return vbox;
-  }
 
   private void createBoard() {
     for (int i = 0; i < NUMBER_OF_TILES; i++) {
