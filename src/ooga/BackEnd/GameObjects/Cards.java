9253d2b83500918cebb8e7b3d66eefa8324c88d4
Rodrigo <rea20@duke.edu>
2020-04-12 07:06:24 -0500

    added functionality for all community chest cards

--- src/ooga/BackEnd/GameObjects/Cards.java
+++ src/ooga/BackEnd/GameObjects/Cards.java
@@ -114,7 +114,7 @@ public class Cards {
                     user.setJailed();
                     break;
                 case 11:
-                    System.out.println("Make general repairs on all your property: For each house pay $25, For each hotel $100.");
+                    System.out.println("Make general repairs on all your property: For each house pay $25, For each hotel $125.");
                     user.payBank(user.getHouses() * 25);
                     break;
                 case 12:
@@ -164,38 +164,81 @@ public class Cards {
             int probality = (int) (Math.random() * 17) + 1;
             switch (probality) {
                 case 1:
+                    System.out.println("Advance to \"GO\". (Collect $200)");
+                    user.receive(CROSS_GO);
+                    user.moveTo(GO_INDEX);
                     break;
                 case 2:
+                    System.out.println("Bank error in your favor. Collect $200.");
+                    user.receive(CROSS_GO);
                     break;
                 case 3:
+                    System.out.println("Doctor's fees. Pay $50.");
+                    user.payBank(50);
                     break;
                 case 4:
+                    System.out.println("From sale of stock you get $50. ");
+                    user.receive(50);
                     break;
                 case 5:
+                    System.out.println("Get Out of Jail Free. This card may be kept until needed or sold/traded.");
+                    user.getJailFreeCard();
                     break;
                 case 6:
+                    System.out.println("Go to Jail. Go directly to jail. Do not pass Go, Do not collect $200.");
+                    user.setJailed();
                     break;
                 case 7:
+                    System.out.println("Grand Opera Night. Collect $50 from every player for opening night seats.");
+                    for (Player p : players) {
+                        if (user != p) {
+                            p.payPlayer(user, 50);
+                        }
+                    }
                     break;
                 case 8:
+                    System.out.println("Holiday Fund matures. Receive $100.");
+                    user.receive(100);
                     break;
                 case 9:
+                    System.out.println("Income tax refund. Collect $20.");
+                    user.receive(20);
                     break;
                 case 10:
+                    System.out.println("It is your birthday. Collect $10 from every player.");
+                    for (Player p : players) {
+                        if (user != p) {
+                            p.payPlayer(user, 10);
+                        }
+                    }
                     break;
                 case 11:
+                    System.out.println("Life insurance matures. Collect $100");
+                    user.receive(100);
                     break;
                 case 12:
+                    System.out.println("Hospital Fees. Pay $50.");
+                    user.payBank(50);
                     break;
                 case 13:
+                    System.out.println("School Fees. Pay $50.");
+                    user.payBank(50);
                     break;
                 case 14:
+                    System.out.println("Receive $25 consultancy fee.");
+                    user.receive(25);
                     break;
                 case 15:
+                    System.out.println("You are assessed for street repairs: Pay $40 per house and $200 per hotel you own.");
+                    user.payBank(user.getHouses() * 40);
                     break;
                 case 16:
+                    System.out.println("You have won second prize in a beauty contest. Collect $10.");
+                    user.receive(10);
                     break;
                 case 17:
+                    System.out.println("You inherit $100.");
+                    user.receive(100);
                     break;
             }
         }
