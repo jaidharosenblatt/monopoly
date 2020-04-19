9615a60f28c4f10739b87268791db76af57fcb43
Austin Odell <awo6@duke.edu>
2020-04-18 21:28:17 -0400

    Refactored the addTitle method into this super-class. Also changed the parameter of the update method to be a list of objects instead of of strings

--- src/ooga/view/DisplayTab.java
+++ src/ooga/view/DisplayTab.java
@@ -1,17 +1,33 @@
 package ooga.view;
 
 import java.util.List;
+
+import javafx.geometry.Pos;
 import javafx.scene.control.Tab;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 public abstract class DisplayTab {
   Tab myTab;
+  Pane myPane;
   public DisplayTab(String tabName, Pane pane){
     myTab = new Tab(tabName, pane);
+    myPane = pane;
   }
 
-  abstract void updateTab(List<String> info);
+  abstract void updateTab(List<Object> info);
 
   protected Tab getTab(){return myTab;}
+
+  protected void addTitle(String str) {
+    HBox hbox = new HBox();
+    Text title = new Text(str);
+    title.setFont(new Font(20));
+    hbox.setAlignment(Pos.CENTER);
+    hbox.getChildren().add(title);
+    myPane.getChildren().add(hbox);
+  }
 
 }
