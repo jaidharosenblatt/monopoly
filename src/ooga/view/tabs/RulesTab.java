ae84dc62a1411f6c202a9d1fcf8447dcdfae656c
Austin Odell <awo6@duke.edu>
2020-04-21 22:00:00 -0400

    Word wrapping for rules

--- src/ooga/view/tabs/RulesTab.java
+++ src/ooga/view/tabs/RulesTab.java
@@ -19,27 +19,30 @@ public class RulesTab extends DisplayTab {
     super(tabName, pane);
     this.myPane = pane;
     super.addTitle(TITLE);
+    addText("a (10 points).Find a fair gamble with two outcomes that would strictlydecrease Bob’s expected utility.b (10 points).Find a fair gamble with two outcomes that would strictlyincrease Bob’s expected utility.2. Finding Nash equilibria of normal-form games. (50 points.)Findallthe Nash equilibria of each of the following five two-player normal-form games.  Argue why the games have no other Nash equilibria.  (Hint:  forsome of these games, you may wish to use strict dominance or iterated strictdominance, because any strategy eliminated by (iterated) strict dominance can-not get positive probability in any Nash equilibrium.  Also keep in mind thatyou may want to use strict dominance by a mixed strategy.)");
+    addText("a (10 points).Find a fair gamble with two outcomes that would strictlydecrease Bob’s expected utility.b (10 points).Find a fair gamble with two outcomes that would strictlyincrease Bob’s expected utility.2. Finding Nash equilibria of normal-form games. (50 points.)Findallthe Nash equilibria of each of the following five two-player normal-form games.  Argue why the games have no other Nash equilibria.  (Hint:  forsome of these games, you may wish to use strict dominance or iterated strictdominance, because any strategy eliminated by (iterated) strict dominance can-not get positive probability in any Nash equilibrium.  Also keep in mind thatyou may want to use strict dominance by a mixed strategy.)");
+    addText("a (10 points).Find a fair gamble with two outcomes that would strictlydecrease Bob’s expected utility.b (10 points).Find a fair gamble with two outcomes that would strictlyincrease Bob’s expected utility.2. Finding Nash equilibria of normal-form games. (50 points.)Findallthe Nash equilibria of each of the following five two-player normal-form games.  Argue why the games have no other Nash equilibria.  (Hint:  forsome of these games, you may wish to use strict dominance or iterated strictdominance, because any strategy eliminated by (iterated) strict dominance can-not get positive probability in any Nash equilibrium.  Also keep in mind thatyou may want to use strict dominance by a mixed strategy.)");
+    addText("a (10 points).Find a fair gamble with two outcomes that would strictlydecrease Bob’s expected utility.b (10 points).Find a fair gamble with two outcomes that would strictlyincrease Bob’s expected utility.2. Finding Nash equilibria of normal-form games. (50 points.)Findallthe Nash equilibria of each of the following five two-player normal-form games.  Argue why the games have no other Nash equilibria.  (Hint:  forsome of these games, you may wish to use strict dominance or iterated strictdominance, because any strategy eliminated by (iterated) strict dominance can-not get positive probability in any Nash equilibrium.  Also keep in mind thatyou may want to use strict dominance by a mixed strategy.)");
+
+    addText("a (10 points).Find a fair gamble with two outcomes that would strictlydecrease Bob’s expected utility.b (10 points).Find a fair gamble with two outcomes that would strictlyincrease Bob’s expected utility.2. Finding Nash equilibria of normal-form games. (50 points.)Findallthe Nash equilibria of each of the following five two-player normal-form games.  Argue why the games have no other Nash equilibria.  (Hint:  forsome of these games, you may wish to use strict dominance or iterated strictdominance, because any strategy eliminated by (iterated) strict dominance can-not get positive probability in any Nash equilibrium.  Also keep in mind thatyou may want to use strict dominance by a mixed strategy.)");
+
   }
+
+
 
   @Override
   void updateTab(List<Object> info) {
-
+    myPane.getChildren().clear();
+    addTitle(TITLE);
+    for (Object rule: info){
+      try{
+        addText((String) rule);
+      }
+      catch (Exception exception){
+        System.out.println("Rules tab only accepts Strings");
+      }
+    }
   }
-
-//  @Override
-//  void updateTab(List<Object> info) {
-//    myPane.getChildren().clear();
-//    addTitle(TITLE);
-//    for (Object rule: info){
-//      try{
-//        addText((String) rule);
-//      }
-//      catch (Exception exception){
-//        System.out.println("Rules tab only accepts Strings");
-//      }
-//
-//    }
-//  }
 
 
 
@@ -49,7 +52,7 @@ public class RulesTab extends DisplayTab {
     HBox hbox = new HBox();
     Text text = new Text(rule);
     text.setFont(new Font(FONT_SIZE));
-    text.maxWidth(Integer.MAX_VALUE);
+    text.setWrappingWidth(250); //TODO:make this not hard-coded
     text.setTextAlignment(TextAlignment.LEFT);
     hbox.getChildren().add(text);
     HBox.setMargin(text, new Insets(0 , 0, 0, 10));
