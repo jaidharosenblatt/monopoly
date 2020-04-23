bd7550b585efd73ba691c676f7264e60a171111f
Austin Odell <awo6@duke.edu>
2020-04-23 11:57:59 -0400

    Refactored the makeColorAndTextHBox method here for use in multiple tabs. Also genericized the update tab to take a collection instead of a list

--- src/ooga/view/tabs/DisplayTab.java
+++ src/ooga/view/tabs/DisplayTab.java
@@ -1,15 +1,24 @@
 package ooga.view.tabs;
 
+import java.util.Collection;
 import java.util.List;
 
 import javafx.geometry.Pos;
 import javafx.scene.control.Tab;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import ooga.BackEnd.GameObjects.Tiles.PropertyTiles.Property;
+import ooga.BackEnd.GameObjects.Tiles.PropertyTiles.Street;
 
 public abstract class DisplayTab {
+  private final double FONT_SIZE = 12;
+  private final double SPACING = 10;
   Tab myTab;
   Pane myPane;
   public DisplayTab(String tabName, Pane pane){
@@ -17,7 +26,7 @@ public abstract class DisplayTab {
     myPane = pane;
   }
 
-  abstract void updateTab(List<Object> info);
+  abstract void updateTab(Collection<Object> info);
 
   protected Tab getTab(){return myTab;}
 
@@ -28,6 +37,24 @@ public abstract class DisplayTab {
     hbox.setAlignment(Pos.CENTER);
     hbox.getChildren().add(title);
     myPane.getChildren().add(hbox);
+  }
+
+  HBox makeColorAndTextHBox(String color, String text, boolean property, Property p) {
+    Circle circle = new Circle(FONT_SIZE /2, Color.web(color));
+    Text playerNum = new Text(text);
+    HBox hBox = new HBox(SPACING);
+    hBox.getChildren().addAll(circle,playerNum);
+    if (property && p instanceof Street) {
+      HBox houses = new HBox(SPACING / 2);
+      for (int i = 0; i < ((Street) p).getHouses(); i++) {
+        ImageView house = new ImageView(new Image(this.getClass().getClassLoader().getResourceAsStream("house.png")));
+        house.setFitHeight(10);
+        house.setFitWidth(10);
+        houses.getChildren().add(house);
+      }
+      hBox.getChildren().add(houses);
+    }
+    return hBox;
   }
 
 }
