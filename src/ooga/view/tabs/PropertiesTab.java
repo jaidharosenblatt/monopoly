748277ead9da665be24aa6555bad3c0fad29caca
Austin Odell <awo6@duke.edu>
2020-04-23 12:02:54 -0400

    Created properties tab to display the current available properties in the bank. It updates its display after each turn during the refreshPlayers method which also updates playersTab

--- src/ooga/view/tabs/PropertiesTab.java
+++ src/ooga/view/tabs/PropertiesTab.java
@@ -1,4 +1,55 @@
 package ooga.view.tabs;
 
-public class PropertiesTab {
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import ooga.BackEnd.GameObjects.Tiles.PropertyTiles.Property;
+
+import java.util.*;
+
+public class PropertiesTab extends DisplayTab{
+    private static final String TITLE = "Available Properties";
+    private static final int SPACING = 10;
+    private static final double FONT_SIZE = 12;
+
+    private List<Property> availableProperty;
+
+
+    private Pane myPane;
+
+    public PropertiesTab(String tabName, Pane pane) {
+        super(tabName, pane);
+        addTitle(TITLE);
+        myPane = pane;
+    }
+
+    protected void initializeProperties(List<Property> properties){
+        availableProperty = new ArrayList<>(properties);
+    }
+
+    @Override
+    void updateTab(Collection<Object> info) {
+        myPane.getChildren().clear();
+        addTitle(TITLE);
+        for (Object obj: info){
+            availableProperty.remove(obj);
+        }
+        displayProperties();
+    }
+
+    private void displayProperties() {
+        VBox vBox = new VBox(SPACING);
+        Collections.sort(availableProperty, new SortByColor());
+        for (Property property : availableProperty){
+            HBox propertyDisplay = makeColorAndTextHBox(property.getGroupColor(),property.getTitle(), false, property);
+            vBox.getChildren().add(propertyDisplay);
+        }
+        myPane.getChildren().add(vBox);
+    }
+    class SortByColor implements Comparator<Property>{
+        @Override
+        public int compare(Property o1, Property o2) {
+            return o1.getGroupNumber() - o2.getGroupNumber();
+        }
+    }
 }
